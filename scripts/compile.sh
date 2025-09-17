442df9c4bb62cd5c8a5edea7b9f94cab0fbd6659
Hazel Rojas <84425557+hazelcode@users.noreply.github.com>
2025-09-17 03:47:35 +0000

    Write out compilation logs into a file instead of terminal. Point to binary

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -2,17 +2,18 @@
 
 cd ../compiler
 
-cmake ./
-make
+cmake ./ > ../scripts/compile.log
+make >> ../scripts/compile.log
 
 rm -rf ../bin
 mkdir ../bin
 cp tea ../bin/tea
 rm tea
 
-echo "TEA binary is in: ../bin"
-
 if [ $# -eq 1 ] && (($1 == "run"))
 then
     ../bin/tea
+else
+    echo "TEA binary is in: ../bin"
+    echo "Compilation logs were generated in: compile.log"
 fi
